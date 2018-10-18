c9f6d146877964bc71810d313c0ee0fd0fd58076
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 21:44:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index do |word, index|
+    puts "#{index + 1}.*#{arr[index]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? do |call|
+     call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(arr)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   arr.find do |cheese|
+     cheese_types.include?(cheese)
+   end
 end
